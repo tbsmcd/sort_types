55e269e31b94c4832e9d16f1ae723013e44b68fb
tbsmcd <tbsmcd@gmail.com>
2022-02-01 21:54:58 +0900

    fix comment

--- scripts/bubble.py
+++ scripts/bubble.py
@@ -1,7 +1,7 @@
 def bubble(rand: list) -> list:
     length = len(rand)
     for i in range(length):
-        # バブルソートは毎回末尾が決まるのでリストの要素数が最大の実行回数
+        # バブルソートは毎回末尾が決まるのでリストの要素数が実行回数
         for j in range(length - i - 1):
             if rand[j] > rand[j + 1]:
                 rand[j], rand[j + 1] = rand[j + 1], rand[j]
