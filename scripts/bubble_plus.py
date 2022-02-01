3298536010d1d923fdf01b78223d4ca41f7a55f2
tbsmcd <tbsmcd@gmail.com>
2022-02-01 21:53:04 +0900

    バブルソートの改良版も追加

--- scripts/bubble_plus.py
+++ scripts/bubble_plus.py
@@ -1,11 +1,15 @@
-def bubble(rand: list) -> list:
+def bubble_plus(rand: list) -> list:
+    # バブルソートの改良版も
     length = len(rand)
     for i in range(length):
         # バブルソートは毎回末尾が決まるのでリストの要素数が最大の実行回数
+        moved = False
         for j in range(length - i - 1):
             if rand[j] > rand[j + 1]:
                 rand[j], rand[j + 1] = rand[j + 1], rand[j]
+                moved = True
+        if not moved:
+            # ソートが発生しなかった場合は終了
+            break
     return rand
 
-
-print(bubble([2, 3, 1, 9, 4, 0, 7]))
