e9d9c327cf0d144f1948d31d12e31116fd38406f
tbsmcd <tbsmcd@gmail.com>
2022-02-02 21:14:01 +0900

    edit comment

--- scripts/quick.py
+++ scripts/quick.py
@@ -7,10 +7,12 @@ import bubble_plus
 # 再帰：分割された区間に対し、再びピボットの選択と分割を行う
 # ソート終了：分割区間が整列済みなら再帰を打ち切る
 # https://ja.wikipedia.org/wiki/%E3%82%AF%E3%82%A4%E3%83%83%E3%82%AF%E3%82%BD%E3%83%BC%E3%83%88
-
+# この説明をもとに実装する
 
 # 擬似的な中央値を返す
 # https://ja.wikipedia.org/wiki/%E3%82%AF%E3%82%A4%E3%83%83%E3%82%AF%E3%82%BD%E3%83%BC%E3%83%88#%E6%9C%80%E6%82%AA%E8%A8%88%E7%AE%97%E9%87%8F%E3%81%AE%E5%9B%9E%E9%81%BF
+
+
 def median(rand: list) -> int:
     if len(rand) <= 2:
         return rand[1]
